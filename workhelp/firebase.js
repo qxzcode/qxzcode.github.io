f6f9fca939ea05a034ff09771ddc8c1d35624dcc
qxzcode <qxzcraft@gmail.com>
2015-02-08 07:18:13 -0800

    Created basic stuff

--- workhelp/firebase.js
+++ workhelp/firebase.js
@@ -1,4 +1,16 @@
-var fb = new Firebase("https://qxzcode1.firebaseio.com/");
-fb.once("value", function(s) {
-	console.log(s.val());
+fb = new Firebase("https://workhelp.firebaseio.com/");
+requestsRef = fb.child("requests");
+
+window.addEventListener("load", function(e) {
+	list = document.getElementById("list");
+});
+
+
+loading = true;
+requestsRef.on("child_added", function(s) {
+	if (loading) {
+		list.innerHTML = "";
+		loading = false;
+	}
+	list.innerHTML += '<div class="reqdiv"><p class="reqtitle">'+s.val().title+'</p><br><p class="reqdesc">'+s.val().desc+'</p></div>';
 });
