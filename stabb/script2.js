da18684aa90fb4fcbfaf640d8f5f3214354c4dd0
qxzcode <qxzcode@users.noreply.github.com>
2016-03-19 13:19:10 -0700

    Update script2.js

--- stabb/script2.js
+++ stabb/script2.js
@@ -2,27 +2,33 @@
 
 function Player(x,y) {
   return {
-    rect: rectCenter(x,y,3,7),
+    rect: rectCenter(x,y,5,10),
     vx:0, vy:0,
     oldX:x, oldY:y,
     onGround: false,
+    joyL:false,joyR:false,joyU:false,joyD:false,
+    btnAtk:false,btnJmp:false,
     frame:
 function(dt,t) {
   var r = this.rect;
-  if (joyL)
+  if (this.joyL)
     r.x -= 100*dt;
-  if (joyR)
+  if (this.joyR)
     r.x += 100*dt;
-  if (btnJmp) {
-    btnJmp = false;
+  if (this.btnJmp) {
+    this.btnJmp = false;
     if (this.onGround)
       this.vy = 300;
   }
   this.vy -= 1000*dt;
+  r.x += this.vx*dt;
   r.y += this.vy*dt;
   this.doCollide();
   
-  drawRect(r,[0,1,0]);
+  bindTex(null);
+  drawRect(r);
+  var a = this.joyD?Math.sin(t*8):0;
+  drawSwordHeld(r.x+4,r.y+2,a/2);
   return false;
 },
   doCollide:
@@ -55,3 +61,25 @@ function() {
   };
 }
 
+var drawSwordCenter,drawSwordHeld;
+function initSword() {
+  var rtt = createRTT(16,16);
+  var r = rectCorner(0,0,16,16);
+  drawSwordCenter = function(x,y,a) {
+    rtt.start();
+    mat4.translate(tMat,tMat,[8,8,0]);
+    mat4.rotateZ(tMat,tMat,a);
+    setTMat();
+    bindTex(solidTex);
+    drawRect(rectCenter(0,0,8,0.5),[0.8,0.8,0.8]);
+    drawRect(rectCenter(-5.5,0,0.5,1.5),[0.8,0.8,0.8]);
+    rtt.stop();
+    bindTex(rtt.tex);
+    r.x = x;
+    r.y = y;
+    drawRect(r);
+  }
+  drawSwordHeld = function(x,y,a) {
+    drawSwordCenter(x+Math.cos(a)*5,y+Math.sin(a)*5,a);
+  }
+};
