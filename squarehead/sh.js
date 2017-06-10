a0980d71d42db3fbaa39a30737adc3c7e601c94b
qxzcode <qxzcraft@gmail.com>
2017-06-10 14:08:18 -0700

    Take advantage of HiDPI displays

--- squarehead/sh.js
+++ squarehead/sh.js
@@ -1,16 +1,23 @@
 /* Squarehead */
- 
- 
+
+
 /* create the document, canvas, and rendering context */
-document.documentElement.innerHTML="<head><meta name='apple-mobile-web-app-capable' content='yes'><meta name = 'viewport' content = 'width = device-width, user-scalable = no'><link rel='apple-touch-icon' href='___APP_ICON___'><title>Squarehead</title></head><body style='background-color:gray;color:black;margin:0;padding:0;'><canvas id='c'></canvas></body>";
-var canvas = document.getElementById("c");
-canvas.width = 1024;
-canvas.height = 1024;
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
 var winWidth, winHeight;
 setWH();
-function setWH() {window.scrollTo(0, 0);winWidth=window.innerWidth;winHeight=window.innerHeight;}
-var ctx = canvas.getContext("2d");
- 
+function setWH() {
+    window.scrollTo(0, 0);
+    winWidth = window.innerWidth;
+    winHeight = window.innerHeight;
+    var ratio = window.devicePixelRatio || 1;
+    canvas.width  = winWidth*ratio;
+    canvas.height = winHeight*ratio;
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.scale(ratio, ratio);
+}
+
 /* input events */
 canvas.addEventListener("touchstart", touchStart, false);
 canvas.addEventListener("touchmove", touchMove, false);
@@ -160,16 +167,16 @@ function newJoystick() {
         }
         };
 }
- 
+
 /* drawing utility functions */
 function drawCircle(x,y,w,h,color,ct){ct=ct||ctx;ct.fillStyle=color;ct.beginPath();ct.arc(x,y,w,h, -9,9);ct.fill();}function drawRect(x,y,w,h,color,ct){ct=ct||ctx;ct.fillStyle=color;ct.fillRect(x,y,w,h);}function drawLine(x1,y1,x2,y2,color,ct){ct=ct||ctx;ct.strokeStyle=color;ct.beginPath();ct.moveTo(x1,y1);ct.lineTo(x2,y2);ct.stroke();}function drawText(text,x,y,ct){ct=ct||ctx;ct.textAlign="center";ct.textBaseline="middle";ct.font=floor(winHeight/18)+"px Arial";ct.fillStyle=document.body.style.color;ct.fillText(text,x,y);}function clearCanvas(ct){ct=ct||ctx;ct.fillStyle=document.body.style.backgroundColor;ct.fillRect(0,0,ct.canvas.width,ct.canvas.height);}function renderToBuffer(w,h,func,b){b=b||document.createElement('canvas');b.width=w;b.height=h;func(b.getContext('2d'));return b;}function drawImage(img,x,y,ct){ct=ct||ctx;ct.drawImage(img,x,y);}
- 
+
 /* cookie handling */
 function setCookie(name,value){if(location.protocol=="data:")return;var b=new Date;b.setTime(b.getTime()+31536E6);document.cookie=name+"="+value+"; "+"expires="+b.toGMTString()}function getCookie(name){if(location.protocol=="data:")return null;var a=name+"=";for(var d=document.cookie.split(";"),b=0;b<d.length;b++){var c=d[b].trim();if(0==c.indexOf(a))return c.substring(a.length,c.length)}return null}
- 
- 
+
+
 /**** VARIABLES ****/
- 
+
 var maps = [
 {name:"The Fort",
 tiles:[
@@ -913,7 +920,7 @@ tiles:[
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 },
-{name:"Ender’s Game",
+{name:"Ender's Game",
 tiles:[
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,0,0,1,5,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,1,0,0,1],
@@ -1001,7 +1008,7 @@ tiles:[
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]}
 ];
- 
+
 var gridWidth, gridHeight;
 var tiles, zombieGrid;
 var portals, ammoDrops;
@@ -1043,10 +1050,10 @@ function setTile(x,y,t) {
     tiles[x][y] = t;
     tilesChanged = true;
 }
- 
+
 /* pathfinding */
 function astar(n,m,q,r){function w(g){var h=[],k=g.x;g=g.y;var e=!1,b=!1,c=!1,f=!1,a;d[k-1]&&(a=d[k-1][g])&&!a.isWall&&(b=!0,a.cost=1+a.z,h.push(a));d[k+1]&&(a=d[k+1][g])&&!a.isWall&&(e=!0,a.cost=1+a.z,h.push(a));d[k]&&(a=d[k][g-1])&&!a.isWall&&(f=!0,a.cost=1+a.z,h.push(a));d[k]&&(a=d[k][g+1])&&!a.isWall&&(c=!0,a.cost=1+a.z,h.push(a));d[k-1]&&(a=d[k-1][g-1])&&!a.isWall&&b&&f&&(a.cost=1.4+a.z,h.push(a));d[k+1]&&(a=d[k+1][g-1])&&!a.isWall&&e&&f&&(a.cost=1.4+a.z,h.push(a));d[k-1]&&(a=d[k-1][g+1])&&!a.isWall&&b&&c&&(a.cost=1.4+a.z,h.push(a));d[k+1]&&(a=d[k+1][g+1])&&!a.isWall&&e&&c&&(a.cost=1.4+a.z,h.push(a));return h}function x(){var g=e[0],h=e.pop();if(0<e.length){e[0]=h;for(var h=0,b=e.length,c=e[h],d=c.f;;){var f=h+1<<1,l=f-1,a=null,m;l<b&&(m=e[l].f,m<d&&(a=l));f<b&&e[f].f<(null==a?d:m)&&(a=f);if(null!=a)e[h]=e[a],e[a]=c,h=a;else break}}return g}function p(g){for(var b=e[g];0<g;){var c=(g+1>>1)-1,d=e[c];if(b.f<d.f)e[c]=b,e[g]=d,g=c;else break}}function s(b,c,d,e){var f=sqrt(2);b=abs(d-b);c=abs(e-c);return 1*(b+c)+(f-2)*Math.min(b,c)}function t(b){for(var c=[];b.parent;)c.push(b),b=b.parent;return c.reverse()}for(var d=[],b=0;b<gridWidth;b++){d[b]=[];for(var f=0;f<gridHeight;f++){var l=tiles[b][f];d[b][f]={x:b,y:f,isWall:0!=l&&2!=l&&4!=l&&5!=l,f:0,g:0,h:0,z:0==zombieGrid[b][f]?0:2,visited:!1,closed:!1,parent:null}}}var e=[],b=d[n][m];n=d[q][r];m=b;b.h=s(b.x,b.y,n.x,n.y);e.push(b);for(p(e.length-1);0<e.length;){b=x();if(b.x==q&&b.y==r)return t(b);b.closed=!0;for(var f=w(b),l=0,y=f.length;l<y;l++){var c=f[l];if(!c.closed){var u=b.g+c.cost,v=c.visited;if(!v||u<c.g){c.visited=!0;c.parent=b;c.h=c.h||s(c.x,c.y,n.x,n.y);c.g=u;c.f=c.g+c.h;if(c.h<m.h||c.h==m.h&&c.g<m.g)m=c;v?p(e.indexOf(c)):(e.push(c),p(e.length-1))}}}}return t(m)}
- 
+
 var enemies;
 var round, zombieTotal, zombiesLeft, zombieTimer, zombieRate;
 function initGameVars() {
@@ -1125,7 +1132,7 @@ function newZombie(x,y) {
         }
     };
 }
- 
+
 var entities;
 var barrels;
 var gasolines;
@@ -1162,7 +1169,7 @@ function spawnAmmoPack(x,y,drop) {
     entities.push(pack);
     return pack;
 }
- 
+
 function initWeapons() {
     return [
         {
@@ -1544,7 +1551,7 @@ function igniteGas(tx,ty,player) {
     setTile(tx,ty,5);
     gasolines.push({x:tx+0.5,y:ty+0.5,p:player,size:0.5,age:rand()*20-10});
 }
- 
+
 var player1,player2, coOp = getCookie("sh_coOp")=="1";
 function initPlayer(pos) {
     return {
@@ -1626,7 +1633,7 @@ function initPlayer(pos) {
         }
     };
 }
- 
+
 function doCollide(e,func) {
     var newX = e.x, newY = e.y;
     e.x = e.lastX; e.y = e.lastY;
@@ -1731,7 +1738,7 @@ function rand() {
 function remove(arr,item) {
     arr.splice(arr.indexOf(item),1);
 }
- 
+
 /* start the main loop! */
 function initGame(mapI) {
     var playersPos=initTiles(mapI);
@@ -1769,7 +1776,7 @@ function drawBg(buf) {
 var gameState = 0, currentMap = getCookie("sh_currentMap")||0, redraw = true;
 currentMap *= 1;
 frame();
- 
+
 /* main everything function - called every frame */
 function frame() {try{
     
